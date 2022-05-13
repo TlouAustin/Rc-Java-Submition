c61cf71aa2f95a8272ca760786833410d900cb38
ST10105678 <ST10105678@RCLMPOCR2PC42.student.advtech.co.za>
2022-05-13 14:24:31 +0200

    Trail run

---
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,12 @@
+
+package javaapplication17;
+
+public class JavaApplication17 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Ziyakhala this side");  
+    }
+    
+}
